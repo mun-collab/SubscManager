0c50e7f3582658b74433d040ba6dbcf30fb73da5
文悠賢 <yuu@ip-10-95-3-159.ap-northeast-1.compute.internal>
2026-09-02 17:08:39 +0900

    feat: Create Subscription model class

--- src/model/Subscription.java
+++ src/model/Subscription.java
@@ -1,5 +1,59 @@
 package model;
 
 public class Subscription {
+	private int id;
+	private String name;
+	private int price;
+	private String category;
+	private int renewalDay;
 
+	// コンストラクタ
+	public Subscription(int id, String name, int price, String category, int renewalDay) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		this.renewalDay = renewalDay;
+	}
+
+	// Getter と Setter
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getRenewalDay() {
+		return renewalDay;
+	}
+
+	public void setRenewalDay(int renewalDay) {
+		this.renewalDay = renewalDay;
+	}
 }
